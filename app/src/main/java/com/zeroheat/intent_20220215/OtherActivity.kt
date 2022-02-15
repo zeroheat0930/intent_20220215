514457bc09e93deee346ab1f5d41a8659a6a6f8a
zeroheat0930 <dongjunkorea@naver.com>
2022-02-15 16:59:27 +0900

    finish() 로 이전 화면으로 복귀

--- app/src/main/java/com/zeroheat/intent_20220215/OtherActivity.kt
+++ app/src/main/java/com/zeroheat/intent_20220215/OtherActivity.kt
@@ -1,11 +1,24 @@
 package com.zeroheat.intent_20220215
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturnToMain.setOnClickListener {
+
+//            돌아가는 경우에는 Intent 활용 x
+//
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+            finish()
+        }
     }
 }
