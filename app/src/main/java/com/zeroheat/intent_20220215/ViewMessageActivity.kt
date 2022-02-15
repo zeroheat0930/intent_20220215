2a099694c5d1a64bce93e60e576bda863bdccbb2
zeroheat0930 <dongjunkorea@naver.com>
2022-02-15 17:16:02 +0900

    이전 화면에서 보낸 문장을 받아서 활용

--- app/src/main/java/com/zeroheat/intent_20220215/ViewMessageActivity.kt
+++ app/src/main/java/com/zeroheat/intent_20220215/ViewMessageActivity.kt
@@ -2,10 +2,18 @@ package com.zeroheat.intent_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
+
+//        화면이 시작되면 바로 문구 설정
+//        이전 화면에서 첨부한 문구 받기("message" 이름표를 붙여서) 첨부한 문구 받기
+
+        val message = intent.getStringExtra("message")
+
+        txtMessage.text = message
     }
 }
