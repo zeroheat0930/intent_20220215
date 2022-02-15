afd6d8ecaabd3b524f9ca1d6d48847b1863cd7ce
zeroheat0930 <dongjunkorea@naver.com>
2022-02-15 16:51:07 +0900

    Intent로 화면 이동

--- app/src/main/java/com/zeroheat/intent_20220215/MainActivity.kt
+++ app/src/main/java/com/zeroheat/intent_20220215/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.zeroheat.intent_20220215
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnBoveToOtherActivity.setOnClickListener {
+
+//            otheractivity로 이동
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
