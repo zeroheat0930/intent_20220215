91bb364801f8f1940b6b3422d77fab9071e4b607
zeroheat0930 <dongjunkorea@naver.com>
2022-02-16 09:48:41 +0900

    닉네임 변경 화면 추가

---
+++ app/src/main/java/com/zeroheat/intent_20220215/EditNicknameActivity.kt
@@ -0,0 +1,11 @@
+package com.zeroheat.intent_20220215
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+    }
+}
